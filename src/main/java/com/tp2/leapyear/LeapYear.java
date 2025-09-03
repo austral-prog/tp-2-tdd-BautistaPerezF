549c84ae972965f881901f431f28977f9471d167
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-09-03 14:29:48 -0300

    Update LeapYear.java

--- src/main/java/com/tp2/leapyear/LeapYear.java
+++ src/main/java/com/tp2/leapyear/LeapYear.java
@@ -3,4 +3,18 @@ package com.tp2.leapyear;
 
 public class LeapYear {
 
+    public boolean DivisibleByFour(int year){
+        if (year%4==0){
+            return true;
+        }return false;
+    }
+    public boolean NotDivisibleByHundred(int year){
+        if (year%100!=0){
+            return true;
+        }return false;
+    }
+    public boolean DivisibleByFourHundred(int year){
+        if (year%400==0){
+            return true; }
+        return false; }
 }
