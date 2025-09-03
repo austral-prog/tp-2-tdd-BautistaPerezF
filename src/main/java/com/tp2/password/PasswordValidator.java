e406b2fef0ccced5daf52e30f7f287c76d3f0c7f
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-09-03 14:31:33 -0300

    Update PasswordValidator.java

--- src/main/java/com/tp2/password/PasswordValidator.java
+++ src/main/java/com/tp2/password/PasswordValidator.java
@@ -1,15 +1,64 @@
 package com.tp2.password;
 
 public class PasswordValidator {
-    
-    public boolean isValid(String password) {
-        // TODO: Implement password validation following TDD approach
-        // Consider these criteria:
-        // - Minimum 8 characters
-        // - At least 1 uppercase letter
-        // - At least 1 lowercase letter  
-        // - At least 1 number
-        // - At least 1 special character (!@#$%^&*()_+-=[]{}|;:,.<>?)
+
+    public boolean Characters(String password) {
+        if (password == null) return false;
+        if (password.length() < 8) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean ContainsUpper(String palabra) {
+        if (palabra == null) return false;
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = palabra.charAt(i);
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean ContainsLower(String palabra) {
+        if (palabra == null) return false;
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = palabra.charAt(i);
+            if (Character.isLowerCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean ContainsNumber(String palabra) {
+        if (palabra == null) return false;
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = palabra.charAt(i);
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean ContainsSpecial(String palabra) {
+        if (palabra == null) return false;
+        String specials = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/";
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = palabra.charAt(i);
+            if (specials.indexOf(c) != -1) {
+                return true;
+            }
+        }
         return false;
     }
 }
+
+// Consider these criteria:
+// - Minimum 8 characters
+// - At least 1 uppercase letter
+// - At least 1 lowercase letter
+// - At least 1 number
+// - At least 1 special character (!@#$%^&*()_+-=[]{}|;:,.<>?)
