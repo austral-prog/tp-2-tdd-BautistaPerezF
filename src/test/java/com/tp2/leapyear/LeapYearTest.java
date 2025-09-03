19e09e7012174fa3513eccbfb85f348f35dea104
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-09-03 14:30:36 -0300

    Update LeapYearTest.java

--- src/test/java/com/tp2/leapyear/LeapYearTest.java
+++ src/test/java/com/tp2/leapyear/LeapYearTest.java
@@ -5,11 +5,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LeapYearTest {
 
-    // TODO: Replace these lines with your tests
+
     @Test
     void exampleTest(){
         assertEquals(4, 2 + 1);
     }
+
+    @Test
+    void TestDivisibleByFour(){
+        LeapYear object = new LeapYear();
+        boolean Value = object.DivisibleByFour(2000);
+        assertEquals(true, Value );
+    }
+    @Test
+    void TestNotDivisibleByHundred(){
+        LeapYear object = new LeapYear();
+        boolean Value = object.NotDivisibleByHundred(52);
+        assertEquals(true, Value );
+    }
+    @Test
+    void TestDivisibleByFourHundred(){
+        LeapYear object = new LeapYear();
+        boolean Value = object.DivisibleByFourHundred(400);
+        assertEquals(true, Value );
+    }
+
+
+
+
+
+
+
+
 
 //    Missing tests:
 //
