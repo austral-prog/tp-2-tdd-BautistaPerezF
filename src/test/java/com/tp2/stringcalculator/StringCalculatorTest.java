cd87ec68e4eb6a2920fe8c71ab50ae61e71b0a9d
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-09-03 14:29:03 -0300

    Update StringCalculatorTest.java

--- src/test/java/com/tp2/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/tp2/stringcalculator/StringCalculatorTest.java
@@ -6,11 +6,44 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest {
 
-    // TODO: Replace these lines with your tests
     @Test
     void exampleTest(){
-        assertEquals(4, 2 + 1);
+        assertEquals(4, 2 + 2);
     }
+    @Test
+    void TestEmptyString(){
+        StringCalculator object = new StringCalculator();
+        assertEquals(0, object.add(""));
+    }
+    @Test
+    void TestSingleNumber(){
+        StringCalculator object = new StringCalculator();
+        assertEquals(4, object.add("4"));
+    }
+    @Test
+    void TestSumOFTwoNumbers(){
+        StringCalculator object = new StringCalculator();
+        assertEquals(7, object.add("3,4"));
+    }
+    @Test
+    void TestMultiplicationOfMultipleNumbers(){
+        StringCalculator object = new StringCalculator();
+        assertEquals(6, object.add("1,2,3"));
+    }
+    @Test
+    void NumbersSeparated(){
+        StringCalculator object = new StringCalculator();
+        assertEquals(6, object.add("1\n2,3"));
+    }
+    @Test
+    void NegativeNumbers(){
+        StringCalculator object = new StringCalculator();
+        assertThrows(IllegalArgumentException.class, () -> object.add("-1"));
+
+    }
+
+
+
 
 //    Missing tests:
 //
